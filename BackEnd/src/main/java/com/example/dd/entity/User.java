e197f8f1f46ecff91a9c5e3173b245d9f46cbe7a
tdwic <wickramanayakatharindu@gmail.com>
2021-06-13 14:08:38 +0530

    entity updated

--- BackEnd/src/main/java/com/example/dd/entity/User.java
+++ BackEnd/src/main/java/com/example/dd/entity/User.java
@@ -4,14 +4,16 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collation = "user")
 public class User {
-
     private int UserID;
     private String UserName;
+    private String Password;
+    private String FirstName;
+    private String LastName;
+    private String PhoneNumber;
 
     public int getUserID() {
         return UserID;
     }
-
     public void setUserID(int userID) {
         UserID = userID;
     }
@@ -19,8 +21,35 @@ public class User {
     public String getUserName() {
         return UserName;
     }
-
     public void setUserName(String userName) {
         UserName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
     }
 }
