d68b5f67e8b3ec8583f1676ed0a82a94d891698f
Dewsara <dewsara6@gmail.com>
2021-06-15 03:30:15 +0530

    Changes to the Header Section

--- Front-End/LunchBuddies/lib/UIHeader.dart
+++ Front-End/LunchBuddies/lib/UIHeader.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'SecondScreen.dart';
+
 class UIHeader extends StatefulWidget {
   @override
   _UIHeaderState createState() => _UIHeaderState();
@@ -8,18 +10,33 @@ class UIHeader extends StatefulWidget {
 class _UIHeaderState extends State<UIHeader> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(children: <Widget>[
-        Container(
-          height: MediaQuery.of(context).size.height * .3,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/Header_2.png')
-                )
-              ),
-        )
-      ]),
-    );
+    return Stack(children: <Widget>[
+      Container(
+        height: MediaQuery.of(context).size.height * .3,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            alignment: Alignment.topCenter,
+            image: AssetImage('assets/images/Header_2.png'),
+          ),
+        ),
+      ),
+      SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: <Widget>[
+            Container(
+              alignment: Alignment.topLeft,
+              child: Row(children: <Widget>[
+                CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 20,
+                    child: Icon(Icons.home_filled, color: Colors.black)),
+              ]),
+            )
+          ]),
+        ),
+      ),
+    ]);
   }
 }
