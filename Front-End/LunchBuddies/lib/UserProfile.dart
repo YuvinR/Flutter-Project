8f8a7e48330041a7b94e79306dcc5044c51ed7e6
Dewsara <dewsara6@gmail.com>
2021-06-21 16:31:57 +0530

    User Details Update crud completed

--- Front-End/LunchBuddies/lib/UserProfile.dart
+++ Front-End/LunchBuddies/lib/UserProfile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'UIHeader.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProfileScreen extends StatefulWidget {
   @override
@@ -8,8 +11,89 @@ class UserProfileScreen extends StatefulWidget {
 }
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  var nameController = TextEditingController();
+  var passwordController = TextEditingController();
+  var phonenumberController = TextEditingController();
+  var loginUsrId ;
+  var loginUsrName;
+    var loginUsrPassword;
+    var loginUsrPhone;
+
+   Future getUserDetails() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userNameInLogin = prefs.getString('usrName') ?? '';
+
+    final response = await http.get(
+      Uri.parse('https://lunbu.herokuapp.com/userByUserName/' + userNameInLogin),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      
+    );
+
+    print(response.body.toString());
+
+    var userDet = json.decode(response.body);
+    loginUsrName = userDet['userName'];
+    loginUsrPassword = userDet['password'];
+    loginUsrPhone = userDet['phoneNumber'];
+    loginUsrId = userDet['userID'];
+
+
+    if (response.statusCode == 200) {
+      nameController.text = loginUsrName;
+      passwordController.text = loginUsrPassword;
+      phonenumberController.text = loginUsrPhone;
+    } else {
+      throw Exception('No User Find');
+    }
+  }
+  Future updateUserDetails() async{
+
+    final response = await http.post(
+      Uri.parse('https://lunbu.herokuapp.com/newUser'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'userName' :loginUsrName,
+        'password': passwordController.value.text,
+        'phoneNumber' :phonenumberController.value.text,
+        'userID':loginUsrId
+      }),
+      
+    );
+
+    var userUpdDet = json.decode(response.body);
+    
+    if (response.statusCode == 200) {
+      if (userUpdDet != null) {
+        print('Updated');
+        final snackBar = SnackBar(
+          content: Text('Profile Has been updated'),
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }else{
+        print('Could not complete');
+      }
+    } else {
+      throw Exception('No User Find');
+    }
+
+  }
+
+  @override
+    void initState() {
+      super.initState();
+      print('Before the SetState');
+      setState(() {
+        print('Calling GetUserDetails');
+        getUserDetails();
+      });
+      print('After the SetState');
+    }
+  
+  
 
   @override
   Widget build(BuildContext context) {
@@ -69,8 +153,8 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             Container(
               padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
               child: TextField(
-                obscureText: true,
-                controller: passwordController,
+                
+                controller: phonenumberController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Phone Number',
@@ -94,12 +178,13 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
                     style: ElevatedButton.styleFrom(
                       primary: Color(0xFFF08D25),
                     ),
-                    onPressed: () {
-                      final snackBar = SnackBar(
-                        content: Text('Record Has Been Updated'),
-                      );
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    }
+                    onPressed: updateUserDetails
+                    // () {
+                    //   final snackBar = SnackBar(
+                    //     content: Text('Record Has Been Updated'),
+                    //   );
+                    //   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    // }
                     ),
               ),
             ),
