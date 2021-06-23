7a5f8eda14e5363654f26f8d2f6732ee82aa7fe6
agrithmicsdev1 <agrithmicsdev1@gmail.com>
2021-06-23 20:31:05 +0530

    minor changes

--- Front-End/LunchBuddies/lib/ListingAddEdit.dart
+++ Front-End/LunchBuddies/lib/ListingAddEdit.dart
@@ -63,6 +63,7 @@ class MidContainerView extends StatefulWidget {
 
 class MidContainer extends  State<MidContainerView> {
 
+  var  dataArray  = [];
    final format = DateFormat("yyyy-MM-dd HH:mm");
 late List _myActivities;
   late String _myActivitiesResult;
@@ -71,6 +72,7 @@ late List _myActivities;
  void initState() {
     super.initState();
    setInitValues();
+   fetchfriends().then((value) =>setDataArray(value));
     _myActivities = [];
     _myActivitiesResult = '';
   }
@@ -79,6 +81,10 @@ late List _myActivities;
   var friendController = TextEditingController();
   var timeDateController = TextEditingController();
 
+  setDataArray(value){
+    dataArray =value;
+  }
+
  Future setInitValues() async{
     final prefs = await SharedPreferences.getInstance();
     final restname = prefs.getString('restuarant') ?? '';
@@ -86,11 +92,24 @@ late List _myActivities;
 
   }
 
-  
-
-
   Future updateUserDetails() async{
 
+  }
+
+  Future<List<dynamic>> fetchfriends() async {
+    final response = await http.get(
+        Uri.parse('https://lunbu.herokuapp.com/getAllFriends'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        });
+    if (response.statusCode == 200) {
+      dataArray = json.decode(response.body);
+      print(dataArray);
+      return json.decode(response.body);
+      
+    } else {
+      throw Exception('No Friend Found');
+    }
   }
 
   _saveForm() {
@@ -140,38 +159,29 @@ late List _myActivities;
                       return 'Please select one or more options';
                     }
                   },
-                  dataSource: [
-                    {
-                      "display": "Running",
-                      "value": "Running",
+                  dataSource:[ {
+                      "firstName": "Kamal",
+                      "lastname": "Running",
+                    },
+                   {
+                      "firstName": "Nimsara",
+                      "lastname": "Running",
                     },
                     {
-                      "display": "Climbing",
-                      "value": "Climbing",
+                      "firstName": "Sanduni",
+                      "lastname": "Running",
                     },
-                    {
-                      "display": "Walking",
-                      "value": "Walking",
+                     {
+                      "firstName": "Susil",
+                      "lastname": "Running",
                     },
-                    {
-                      "display": "Swimming",
-                      "value": "Swimming",
+                     {
+                      "firstName": "Kavindya",
+                      "lastname": "Running",
                     },
-                    {
-                      "display": "Soccer Practice",
-                      "value": "Soccer Practice",
-                    },
-                    {
-                      "display": "Baseball Practice",
-                      "value": "Baseball Practice",
-                    },
-                    {
-                      "display": "Football Practice",
-                      "value": "Football Practice",
-                    },
-                  ],
-                  textField: 'display',
-                  valueField: 'value',
+                    ],
+                  textField: 'firstName',
+                  valueField: 'firstName',
                   okButtonLabel: 'OK',
                   cancelButtonLabel: 'CANCEL',
                   // required: true,
