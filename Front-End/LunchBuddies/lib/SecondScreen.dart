3a626b78778bfaf98abf0071b45c53a151f6c826
Ovi98 <ovij98@gmail.com>
2021-06-23 23:46:56 +0530

    Adding the header to Home UI

--- Front-End/LunchBuddies/lib/SecondScreen.dart
+++ Front-End/LunchBuddies/lib/SecondScreen.dart
@@ -10,6 +10,29 @@ import 'package:flutter_svg/flutter_svg.dart';
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * .28,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              children: <Widget>[
+                UIHeader(),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Align(
+                    alignment: Alignment(-1, 0.35),
+                    child: Text(
+                      "Home",
+                      style: Theme.of(context).textTheme.headline4!.copyWith(
+                          fontWeight: FontWeight.bold, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]);
   }
 }
